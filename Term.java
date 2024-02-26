58a23b7fababdd66a4723b16dfb1f545025b3c16
k11logram <161375355+k11logram@users.noreply.github.com>
2024-02-26 21:58:11 +0200

    Add files via upload

---
+++ Term.java
@@ -0,0 +1,17 @@
+//Dube Kagiso
+//DBXKAG003
+//26 February 2023
+
+public class Term{
+      String term;
+      Item left;
+      Item right;
+      
+  public Term(String term){
+       this.term=term;
+       left=null;
+       right=null;
+       
+       }
+  }
+      
