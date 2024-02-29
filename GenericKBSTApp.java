ad7b136bc0e6c3d80fd408e1df4a16edd98007ca
k11logram <161375355+k11logram@users.noreply.github.com>
2024-02-29 22:47:57 +0200

    Add files via upload

--- GenericKBSTApp.java
+++ GenericKBSTApp.java
@@ -3,16 +3,32 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Scanner;
 import java.io.IOException;
+import javax.swing.JFrame;
+import java.awt.Color;
 
 public class GenericKBSTApp{
    public static void main(String args[]){
       String input="";
       BST node=new BST();
+      
+      JFrame frame = new JFrame();
+      frame.setVisible(true);
+      frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setTitle("GenericsKBSTApp");
+      frame.getContentPane().setBackground(Color.black);
       do{
-      input=JOptionPane.showInputDialog(null,"Choose an action from the menu:\n1. Load a knowledge base from a file\n2. Add a new statement to the knowledge base\n3. Search for an item in the knowledge base by term\n4. Search for a item in the knowledge base by term and sentence\n5. Quit");
+         do{
+            input=JOptionPane.showInputDialog(frame,"Choose an action from the menu:\n1. Load a knowledge base from a file\n2. Add a new statement to the knowledge base\n3. Search for an item in the knowledge base by term\n4. Search for a item in the knowledge base by term and sentence\n5. Quit");
+        
+            if(input==null){
+            JOptionPane.showMessageDialog(frame,"You neeed to choose option 5 to exit");}      
+         }
+         while (input == null);
+      
        if(input.equals("1")){
        try{
-        String file=JOptionPane.showInputDialog(null,"Enter file name: ");
+        String file=JOptionPane.showInputDialog(frame,"Enter file name: ");
         BufferedReader read = new BufferedReader(new FileReader(file));
       String fileLine ="";
       while((fileLine = read.readLine()) != null){
@@ -29,46 +45,44 @@ public class GenericKBSTApp{
        }
        catch(IOException e){
        // e.printStackTrace();
-        JOptionPane.showMessageDialog(null, "please enter a proper file name or enter proper location of the file");}       
+        JOptionPane.showMessageDialog(frame, "please enter a proper file name or enter proper location of the file");}       
       if(node != null){
-        JOptionPane.showMessageDialog(null,"Knowledge base loaded successfully.");}
+        JOptionPane.showMessageDialog(frame,"Knowledge base loaded successfully.");}
          }
          
        else if(input.equals("2")){
-       String term=JOptionPane.showInputDialog(null,"Enter the term: ")+"\t";
-       String statement=JOptionPane.showInputDialog(null,"Enter the statement: ")+"\t";
-       double confidence= Double.parseDouble(JOptionPane.showInputDialog(null,"Enter the confidence score: "));
+       String term=JOptionPane.showInputDialog(frame,"Enter the term: ")+"\t";
+       String statement=JOptionPane.showInputDialog(frame,"Enter the statement: ")+"\t";
+       double confidence= Double.parseDouble(JOptionPane.showInputDialog(frame,"Enter the confidence score: "));
        Item item = new Item(term,statement,confidence);
        node.Replace(item,node.root);
-       JOptionPane.showMessageDialog(null,"Statement has been added to database.");
+       JOptionPane.showMessageDialog(frame,"Statement has been added to database.");
        
        }
        else if (input.equals("3")){
-       String term=JOptionPane.showInputDialog(null,"Enter the term to search: ")+"\t";
+       String term=JOptionPane.showInputDialog(frame,"Enter the term to search: ")+"\t";
        Term newNode=node.find(term);
-       JOptionPane.showMessageDialog(null,newNode.term.getItem().trim()+" "+newNode.term.getStatement().trim()+" (Confidence score: "+Double.toString(newNode.term.getConfidence())+")");
+       JOptionPane.showMessageDialog(frame,newNode.term.getItem().trim()+" "+newNode.term.getStatement().trim()+" (Confidence score: "+Double.toString(newNode.term.getConfidence())+")");
        }
 
        else if (input.equals("4")){
-       String term=JOptionPane.showInputDialog(null,"Enter the term to search: ")+"\t";
-       String statement=JOptionPane.showInputDialog(null,"Enter the statement to search for: ")+"\t";
+       String term=JOptionPane.showInputDialog(frame,"Enter the term to search: ")+"\t";
+       String statement=JOptionPane.showInputDialog(frame,"Enter the statement to search for: ")+"\t";
        Term newNode=node.find(term);
        if(newNode.term.getStatement().equals(statement) && newNode != null){
-         JOptionPane.showMessageDialog(null, "The statement was found and has a confidence score of "+newNode.term.getConfidence());}
+         JOptionPane.showMessageDialog(frame, "The statement was found and has a confidence score of "+newNode.term.getConfidence());}
        else{
-         JOptionPane.showMessageDialog(null,"Term and statement cannot be found:");}
+         JOptionPane.showMessageDialog(frame,"Term and statement cannot be found:");}
        }
       else if (!(input.equals("1") || input.equals("2") || input.equals("3") || input.equals("4") || input.equals("5"))){
-        JOptionPane.showMessageDialog(null,"choose one of the five options displayed to you");
+        JOptionPane.showMessageDialog(frame,"choose one of the five options displayed to you");
         }
-        
-       else if(input==null){
-        JOptionPane.showMessageDialog(null,"You neeed to choose option 5 to exit");}
-        else{
-        break;
-       }
+   //   else{
+//         break;
+//        }
        }
        
         while(!input.equals("5"));
+        frame.dispose();
       }
 }
